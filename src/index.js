d0d699163b75ceb6072d92841d92072420c1ec00
SimChid <schidley@wesleyan.edu>
2025-03-28 13:52:31 -0400

    'Start of a page'

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Bio from './views/bio'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className='name'>
+      <h1>Simon Chidley</h1>
+            
+    </div>
+    <Bio />
   </React.StrictMode>
 );
 
---
+++ src/views/bio.js
@@ -0,0 +1,17 @@
+function bio() {
+    return(
+        <div class = 'bio'>
+            <p>
+                My name is Simon Chidley, class of '25. I'm a computer science major and IDEAS minor
+                here at Wesleyan University.
+            </p>
+            <p>
+                My concentration in the major was on Computers, but along the way I encountered 
+                other disciplines like electrical engineering and 3D design. Click one of the links
+                below to showcase the projects I completed (or attempted to complete).
+            </p>
+        </div>
+    );
+}
+
+export default bio;
