c939ed59b26f9147305eda46f1a09474b5841832
SimChid <schidley@wesleyan.edu>
2025-03-31 19:21:51 -0400

    Looking better!

--- src/views/modalmenu.js
+++ src/views/modalmenu.js
@@ -2,15 +2,54 @@ import Popup from 'reactjs-popup';
 import './modalmenu.css';
 
 function Menu() {
-    <Popup
-        trigger = {<button className = 'button'>Test</button>}
-        modal
-    >
-        <div>
-            This is a test
-        </div>
-        
-    </Popup>
+    return(
+        <ul className = 'menu'>
+            <li>
+                <Popup
+                    trigger = {<button className = 'button'>Digital Foundations</button>}
+                    modal
+                >
+                    <div>
+                        This is a test
+                    </div>
+                
+                </Popup>
+            </li>
+            <li>
+                <Popup
+                    trigger = {<button className = 'button'>Electrical Engineering</button>}
+                    modal
+                >
+                    <div>
+                        This is another test
+                    </div>
+
+                </Popup>
+            </li>
+            <li>
+                <Popup
+                    trigger = {<button className = 'button'>Digital Media Lab</button>}
+                    modal
+                >
+                    <div>
+                        Yet another test!
+                    </div>
+
+                </Popup>
+            </li>
+            <li>
+                <Popup
+                    trigger = {<button className = 'button'>Sensors and Data Analysis</button>}
+                    modal
+                >
+                    <div>
+                        Voila!
+                    </div>
+
+                </Popup>
+            </li>
+        </ul>
+    )
 };
 
 export default Menu;
