d2b3759870b61869dffc82e05003e86acfa97e9f
SimChid <schidley@wesleyan.edu>
2025-04-12 10:45:19 -0400

    little tweaks

--- src/views/df.js
+++ src/views/df.js
@@ -8,13 +8,7 @@ function Df({ close }) {
             </button>
             <div className="header">ARST-190: Digital Foundations</div>
             <div className="content">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
-                Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
-                delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
-                <br />
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
-                commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
-                explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
+                This page is under construction.
             </div>
         </div>
     );
--- src/views/sda.js
+++ src/views/sda.js
@@ -8,13 +8,7 @@ function Sda({ close }) {
             </button>
             <div className="header">IDEA-215: Sensors, Measurement, and Data Analysis</div>
             <div className="content">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
-                Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
-                delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
-                <br />
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
-                commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
-                explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
+                This page is under construction
             </div>
         </div>
     );
