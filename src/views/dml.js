0c7c0194d1e24a7555f2feda244ef416e4d71255
SimChid <schidley@wesleyan.edu>
2025-04-13 13:28:18 -0400

    Added dml content

--- src/views/dml.js
+++ src/views/dml.js
@@ -1,4 +1,8 @@
 import './modal.css';
+import DiJaws from "./images/dml/DiJaws.jpg";
+import LedVuitton from "./images/dml/LedVuitton.jpg";
+import ScreamDesGarcons from "./images/dml/ScreamDesGarcons.jpg";
+import BigBuys from "./images/dml/BigBuys.jpg";
 
 function Dml({ close }) {
     return (
@@ -8,7 +12,24 @@ function Dml({ close }) {
             </button>
             <div className="header">IDEA-285: Digital Media Lab</div>
             <div className="content">
-                this page is under construction
+                <div className='project'>BigBuys (a collection of vintage t-shirts with designer logos)</div>
+                This was for the assignment titled "Replicas, Forgeries, Counterfeits, and Imposters", where we were basically tasked
+                to steal art through one of the methods above. I used Adobe Photoshop to combine vintage t-shirts with designer brands
+                to hypothetically create the most valuable shirt based on graphic design alone. Below are just some examples:
+                <br/><br />
+                <img src={DiJaws} alt='DiJaws' />
+                <div className='caption'>A design from Dior mashed with the Jaws poster.</div>
+                <br/><br/>
+                <img src={LedVuitton} alt='LedVuiton' />
+                <div className='caption'>A Louis Vuitton shirt with a Led Zeppelin logo placed over it</div>
+                <br /><br />
+                <img src={ScreamDesGarcons} alt='ScreamDesGarcons' />
+                <div className='caption'>The Scream poster with a Comme Des Garcons heart pasted over Neve Campbell's face.</div>
+                <br /><br />
+                Furthermore, in photoshop, I created a fake website based directly off of Etsy, and gave all of this shirts absurd names and
+                prices.
+                <br /><br />
+                <img src={BigBuys} alt='BigBuys' />
             </div>
         </div>
     );
